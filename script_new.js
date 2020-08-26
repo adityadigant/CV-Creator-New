22bae7787058b8921896bbe4c2f1c53165abcfe9
adityadigant <67951710+adityadigant@users.noreply.github.com>
2020-08-26 14:37:47 +0530

    Add files via upload

--- script_new.js
+++ script_new.js
@@ -15,6 +15,7 @@ function openCity(evt, cityName) {
   var loadFile = function(event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
+    document.getElementById("outshow").src=URL.createObjectURL(event.target.files[0]);
   };
 
 
@@ -27,5 +28,51 @@ function openCity(evt, cityName) {
     } else {
         alert("Sorry, your browser does not support HTML5 geolocation.");
     }
+    
 }
 
+
+
+function createresume(){
+  document.getElementById("nameshow").innerHTML=
+ document.getElementById("name").value;
+
+ document.getElementById("addressshow").innerHTML=
+ document.getElementById("address").value;
+
+ document.getElementById("phoneshow").innerHTML="+91 "+
+ document.getElementById("phone").value;
+
+ document.getElementById("emailshow").innerHTML=
+ document.getElementById("email").value;
+
+ document.getElementById("aboutshow").innerHTML="\""+
+ document.getElementById("about").value +"\"";
+
+ document.getElementById("careershow").innerHTML=
+ document.getElementById("career").value;
+ 
+ document.getElementById("edushow").innerHTML=
+ document.getElementById("education").value;
+
+ document.getElementById("recentshow").innerHTML="<strong>Date:</strong> " +
+ document.getElementById("job-1__start").value+" To "+document.getElementById("job-1__end").value;
+
+ document.getElementById("positionshow1").innerHTML="<strong>Position:</strong> " +
+ document.getElementById("job-1__details").value;
+
+ document.getElementById("pastshow").innerHTML="<strong>Date:</strong> " +
+ document.getElementById("job-2__start").value+ " To " +document.getElementById("job-2__end").value ;
+
+ document.getElementById("positionshow2").innerHTML="<strong>Position:</strong> " +
+ document.getElementById("job-2__details").value;
+ 
+ document.getElementById("anotherpastshow").innerHTML="<strong>Date:</strong> " +
+ document.getElementById("job-3__start").value+" To "+document.getElementById("job-3__end").value;
+
+ document.getElementById("positionshow3").innerHTML="<strong>Position:</strong> " +
+ document.getElementById("job-3__details").value;
+
+ document.getElementById("referenceshow").innerHTML=
+ document.getElementById("references").value;
+}
